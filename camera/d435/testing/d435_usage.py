21a62e8c1a63509a6ea03a05439a498103d02fca
yiyeChen <yychen2019@gatech.edu>
2021-10-08 09:47:03 -0400

    The usage  testing script.

---
+++ camera/d435/testing/d435_usage.py
@@ -0,0 +1,31 @@
+"""
+
+    @brief          The basic usage of the d435 runner class
+
+    @author         Yiye Chen.          yychen2019@gatech.edu
+    @date           10/07/2021
+
+"""
+import matplotlib.pyplot as plt
+import camera.d435.d435_runner as d435
+
+import Surveillance.utils.display as display
+
+d435_configs = d435.D435_Configs(
+    W_dep=1280,
+    H_dep=720,
+    W_rgb=1920,
+    H_rgb=1080
+)
+
+d435_starter = d435.D435_Runner(d435_configs)
+
+fh = plt.figure()
+while(True):
+    rgb, dep, status = d435_starter.get_frames()
+    display.display_rgb_dep(rgb, dep, suptitle="THe D435 camera signals. Press any key to end", fh=fh)
+    if plt.waitforbuttonpress(0.01):
+        break
+
+
+
