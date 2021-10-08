233150a5ad6a806285942750f8872a8c0fcf5467
yiyeChen <yychen2019@gatech.edu>
2021-10-07 23:10:36 -0400

    Update the installation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # camera
 Encaspulation of typical camera functionality.
+
+## install
+```
+git clone https://github.com/ivapylibs/camera.git
+pip3 install -e camera/
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='camera',
+  version   = '1.0',
+  description   = 'Classes implementing the runner interface for the commonly used cameras', 
+  url       = "https://github.com/ivapylibs/camera",
+  author    = 'IVALab',
+  packages  = ['camera'],
+  install_requires=['pyrealsense2', 'numpy', 'matplotlib','opencv-python']
+)
