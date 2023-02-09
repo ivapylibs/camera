1faf0276d8596266472f4d9a6d956ccd4c15d21b
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-02-09 14:25:20 -0500

    Add ubuntu 16.04 pinned dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
-setup(name='camera',
-  version   = '1.0',
-  description   = 'Classes implementing the runner interface for the commonly used cameras', 
-  url       = "https://github.com/ivapylibs/camera",
-  author    = 'IVALab',
-  packages  = ['camera'],
-  install_requires=['pyrealsense2', 'numpy', 'matplotlib','opencv-contrib-python==4.5.5.62', \
-    "sklearn", "tqdm"]
+
+setup(
+    name="camera",
+    version="1.1.0",
+    description="Classes implementing the runner interface for the commonly used cameras",
+    url="https://github.com/ivapylibs/camera",
+    author="IVALab",
+    packages=["camera"],
+    install_requires=[
+        "pyrealsense2",
+        "numpy",
+        "matplotlib",
+        "opencv-contrib-python==4.5.5.62",
+        "scikit-learn",
+        "tqdm",
+    ],
+    extras_require={
+      "viz": ["pptk"]
+    }
 )
