844f431e9077e5dff41d410ac5d9f2159f01926b
Patricio A. Vela <pvela@gatech.edu>
2023-08-03 17:50:14 -0400

    Need uncompressed bag. bag01replay works.

--- testing/d435/bag01replay.py
+++ testing/d435/bag01replay.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python
+#============================== bag01replay ==============================
+'''!
+@brief  Snag a frame for a ROS bag recorded using the realsense API.  
+        The bag needs to be decompressed (original bag is decompressed).
+
+Simple test to grab a frame and move on.
+'''
+#============================== bag01replay ==============================
+
+#
+# @author   Patricio A. Vela        pvela@gatech.edu
+# @date     2023/08/03              [from realsense read_bag_example.py]
+#
+#============================== bag01replay ==============================
 
 
+import numpy as np
 import pyrealsense2 as rs
+import cv2
+
 import camera.utils.rs_utils as rs_utils
 
 from camera.d435.runner2 import CfgD435
@@ -11,24 +28,18 @@ import camera.utils.display as display
 pipe   = rs.pipeline()
 config = rs.config()
 config.enable_device_from_file('/home/mary/Downloads/16fd1967d2_Garrison.bag')
-config.enable_stream(rs.stream.color, rs.format.bgr8, 30)
+config.enable_all_streams()
 
-print("Starting")
 profile = pipe.start(config)
-# CRASHES HERE. REALSENSE-VIEWER CRASHES WITH BAG FILES TOO.
-# NOT SURE WHY. GARRISON SHOULD CONFIRM AND PROVIDE VERSION NUMBER.
+# @note Will crash here is the bag file is compressed.
 
-#align = rs.align(rs.stream.color)
-print("Entering loop")
+frame = pipe.wait_for_frames()
+color_frame = frame.get_color_frame()
+color_image = np.asanyarray(color_frame.get_data())
 
-while True:
-  frame = pipe.wait_for_frames()
-  print('Got frame')
-  color_frame = frame.get_color_frame()
-  print('Got color')
+display.rgb_cv(color_image,ratio=0.5,window_name="color")
 
-  display.rgb_cv(color_frame,ratio=0.5,window_name="color")
+opKey = cv2.waitKey(-1)
 
-  opKey = cv2.waitKey(1)
-  if opKey == ord('q'):
-    break
+#
+#============================== bag01replay ==============================
